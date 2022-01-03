f17ab7a21ff91722fdefa862e039debba20f67fa
Vibhu2 <vibhubhatnagar@outlook.com>
2022-01-03 17:24:20 -0500

    script updated

--- PowershellScripts/O365Report.ps1
+++ PowershellScripts/O365Report.ps1
@@ -1,11 +1,20 @@
 # Fetch O365 sync status details of each client 
 # Module AzureAD
 # 
-# 
+# Always use below script in terminal
 
+Import-Module MSOnline,AzureAD,AzureADTenantID,AzureADUserFederation,MSAL.PS,NinjaRmmApi,Office365,Office365Connect,PartnerCenter
 Connect-PartnerCenter
 Connect-MsolService
 $customer = Get-PartnerCustomer
 
-Get-MsolCompanyInformation -TenantId 7c9650fb-4c7b-4298-9eb8-9b61db4b9fb0 | Select-Object -Property DisplayName,DirectorySynchronizationEnabled,LastDirSyncTime,PasswordSynchronizationEnabled,LastPasswordSyncTime | Format-Table -AutoSize
+$syncinfo= Foreach ($cust in $customer){
+  Get-MsolCompanyInformation -TenantId $cust.CustomerId 
+ }
 
+$syncinfo | Select-Object -Property DisplayName,DirectorySynchronizationEnabled,LastDirSyncTime,PasswordSynchronizationEnabled,LastPasswordSyncTime | Format-Table -Expand -AutoSize -Force
+
+<#/ Foreach ($cust in $customer){
+Get-MsolCompanyInformation -TenantId $cust.CustomerId | Select-Object -Property DisplayName,DirectorySynchronizationEnabled,LastDirSyncTime,PasswordSynchronizationEnabled,LastPasswordSyncTime | Format-Table -AutoSize
+ }
+#>
