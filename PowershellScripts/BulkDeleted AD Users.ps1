12d021016285dd5f0c72015785bd6dbed6f0815a
Vibhu2 <vibhubhatnagar@outlook.com>
2022-01-20 17:40:51 -0500

    minor changes

--- PowershellScripts/BulkDeleted AD Users.ps1
+++ PowershellScripts/BulkDeleted AD Users.ps1
@@ -7,5 +7,5 @@ $users = Get-Content "C:\it\Users.txt"
 
 # Checking out users once by one and if found removing them from Active Directory
 foreach ($user in $users) {
-   Get-AdUser -Filter "Name -like '$user'" |   Remove-ADUser -Confirm:$false
+   Get-AdUser -Filter "Name -like '$user'" | Remove-ADUser -Confirm:$false
 } 
