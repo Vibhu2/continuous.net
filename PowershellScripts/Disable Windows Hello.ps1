d06e89fa585066433c0f9b7e1a46067860618197
Vibhu2 <vibhubhatnagar@outlook.com>
2022-03-25 09:37:11 -0400

    Updated scriots

---
+++ PowershellScripts/Disable Windows Hello.ps1
@@ -0,0 +1,4 @@
+﻿Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Settings\AllowSignInOptions" -Name "value" -Value 1
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork" -value “default value”
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork -Name DisablePostLogonProvisioning -Type "DWORD” -Value 0
+
---
+++ PowershellScripts/Distribution list and users report.ps1
@@ -0,0 +1,18 @@
+﻿Connect-ExchangeOnline
+Get-DistributionGroup
+team@iabtechlab.com
+Techlab-bod@iabtechlab.com
+Techlab-bod-ea@iabtechlab.com
+Techlab-executivecomm@iabtechlab.com
+techlab-financecomm@iabtechlab.com
+
+$Distribution= Get-DistributionGroup
+
+ForEach ($object in $Distribution) {
+
+write-host -ForegroundColor Cyan $object
+#Get-DistributionGroupMember -Identity "$object" | Select-Object -Property DisplayName,PrimarySmtpAddress | Format-Wide -Property DisplayName,PrimarySmtpAddress -AutoSize
+Get-DistributionGroupMember -Identity "$object" | Format-table -Property DisplayName,PrimarySmtpAddress | Export-Csv -Path C:\Intel\iab Distributionlist.csv -NoTypeInformation
+}
+
+Get-DistributionGroupMember -Identity "team@iabtechlab.com" | Select-Object -Property DisplayName,PrimarySmtpAddress 
